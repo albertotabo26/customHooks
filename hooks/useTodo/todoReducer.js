22c17c9df3cea0f361fec4a19f5c1db0ad86b982
Alberto Taboada <albertotabogr@gmail.com>
2022-08-22 16:52:07 -0600

    Se agregaron las carpetas y se agrego el todoReducer faltante

---
+++ hooks/useTodo/todoReducer.js
@@ -0,0 +1,25 @@
+export const todoReducer = (initialState, action) => {
+
+    switch (action.type) {
+        case 'add':
+            //throw new Error('No se pudo agregar el TODO');
+            return [...initialState, action.payload];
+        case 'delete':
+            return initialState.filter(todo => todo.id !== action.payload);
+
+        case 'toggle':
+            return initialState.map(todo => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                    
+                return todo;
+                
+            });
+        default:
+            return initialState;
+    }
+}
